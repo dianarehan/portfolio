b15933669a95aeb75f8c60338af06634531d6e67
diana <daiana.rehan.dr@gmail.com>
2025-04-14 00:34:12 +0200

    Updates

---
+++ assets/config-o_T4FUZY.js
@@ -0,0 +1,5 @@
+const n="Diana Rehan",i="Developer",e=["VR Developer","Game Developer"],o="https://hamishw.com",t="dianarehan",s="diana-rehan",c="https://github.com/HamishMW/portfolio",a="https://dianarehan.itch.io/",h=`__  __  __
+\\ \\ \\ \\ \\∕
+ \\ \\∕\\ \\
+  \\∕  \\∕
+`,r={name:n,role:i,disciplines:e,url:o,github:t,linkedin:s,repo:c,itch:a,ascii:h};export{r as c};
