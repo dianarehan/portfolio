ddcf032e7a85f2a83d0af92c1239845d9ef0a187
diana <daiana.rehan.dr@gmail.com>
2026-01-06 14:13:10 +0200

    Fix social image

---
+++ assets/config-Cy2OsFE5.js
@@ -0,0 +1,5 @@
+const n="Diana Rehan",i="Software Engineer",o=["XR Developer","Game Developer"],e="https://dianarehan.github.io/portfolio",t="dianarehan",a="diana-rehan",c="https://github.com/dianarehan/portfolio",r="https://dianarehan.itch.io/",s="https://upwork.com/freelancers/~016e45e5a14c79f483",h=`__  __  __
+\\ \\ \\ \\ \\∕
+ \\ \\∕\\ \\
+  \\∕  \\∕
+`,p={name:n,role:i,disciplines:o,url:e,github:t,linkedin:a,repo:c,itch:r,upwork:s,ascii:h};export{p as c};
--- assets/config-DeZ93xTT.js
+++
@@ -1,5 +0,0 @@
-const n="Diana Rehan",e="Software Engineer",i=["XR Developer","Game Developer"],o="https://dianarehan.pages.dev",a="dianarehan",t="diana-rehan",c="https://github.com/dianarehan/portfolio",s="https://dianarehan.itch.io/",r="https://upwork.com/freelancers/~016e45e5a14c79f483",h=`__  __  __
-\\ \\ \\ \\ \\∕
- \\ \\∕\\ \\
-  \\∕  \\∕
-`,p={name:n,role:e,disciplines:i,url:o,github:a,linkedin:t,repo:c,itch:s,upwork:r,ascii:h};export{p as c};
